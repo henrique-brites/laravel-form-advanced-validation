6d59ff4f1e34af73be1be801f3db238d1626406b
Henrique Brites <henriquebrites@live.com>
2022-01-03 00:23:14 -0400

    feature: flash messages

--- resources/views/admin/clients/create.blade.php
+++ resources/views/admin/clients/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h3>Novo cliente</h3>
+    <h3>Novo cliente {{Session::get('chave')}}</h3>
     <h4>{{$clientType == \App\Client::TYPE_INDIVIDUAL? 'Pessoa Física': 'Pessoa Jurídica'}}</h4>
     <a href="{{route('clients.create',['client_type' => \App\Client::TYPE_INDIVIDUAL])}}">Pessoa Física</a> |
     <a href="{{route('clients.create',['client_type' => \App\Client::TYPE_LEGAL])}}">Pessoa Jurídica</a>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,6 +12,14 @@
         <div class="row">
             <div class="container">
                 <h1>Laravel: Fotmulários e Validações</h1>
+                @if(Session::has('message'))
+                    <div class="alert alert-success alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        {{Session::get('message')}}
+                    </div>
+                @endif
                 @yield('content')
             </div>
         </div>
